5692ef6f5a6f2f69acef03f79eb053f94f3de8f7
Jakub Kulig <jakub.kulig@rocketweb.com>
2018-07-10 11:51:17 +0200

    Refactor product carousel scripts

--- view/frontend/web/js/product-slider.js
+++ view/frontend/web/js/product-slider.js
@@ -9,75 +9,82 @@ define([
             sliderConfig: {},
             breakpoints: {}
         },
-
+        
         _create: function() {
-            var self = this;
-
-            self._initializeSlider();
-        },
-
-        _initializeSlider: function() {
             var self = this,
-                slider = $(this.element),
-                items = ((this.options.sliderConfig.items >= 0 && this.options.sliderConfig.items != null) ? this.options.sliderConfig.items : 2),
-                total = this.options.total,
-                stagePadding = this.options.sliderConfig.stagePadding != '' ? parseInt(this.options.sliderConfig.stagePadding) : 0,
-                animate_Out = this.options.sliderConfig.transition == 'fadeOut' ? true : false;
-                
-                slider.slick({
-                    autoplay: parseInt(self.options.sliderConfig.autoplay) == 1 ? true : false,
-                    autoplaySpeed: (parseInt(self.options.sliderConfig.autoplayTimeout) > 0 && self.options.sliderConfig.autoplayTimeout != null) ? parseInt(self.options.sliderConfig.autoplayTimeout) : 3000,
-                    adaptiveHeight: parseInt(self.options.sliderConfig.autoHeight) == 1 ? true : false,
-                    arrows: parseInt(self.options.sliderConfig.nav) == 1 ? true : false,
-                    dots: (parseInt(self.options.sliderConfig.dots) == 1 && parseInt(items) < total) ? true : false,
-                    lazyLoad: parseInt(self.options.sliderConfig.lazyLoad) == 1 ? "ondemand" : "progressive",
-                    infinite: parseInt(self.options.sliderConfig.loop) == 1 ? true : false,
-                    centerPadding: parseInt(self.options.sliderConfig.center) == 1 ? 0 : stagePadding,
-                    slidesToShow: parseInt(items),
-                    slidesToScroll: parseInt(items),
-                    fade: animate_Out,
-                    responsive: [
-                        {
-                            breakpoint: self.options.breakpoints.breakpoint_1,
-                            settings: {
-                                slidesToShow: (self.options.sliderConfig.items_brk1 >= 0 && self.options.sliderConfig.items_brk1 != null) ? parseInt(self.options.sliderConfig.items_brk1) : parseInt(items),
-                                slidesToScroll: (self.options.sliderConfig.items_brk1 >= 0 && self.options.sliderConfig.items_brk1 != null) ? parseInt(self.options.sliderConfig.items_brk1) : parseInt(items),
-                                arrows: parseInt(self.options.sliderConfig.nav_brk1) == 1 ? true : false,
-                                dots: (self.options.sliderConfig.items_brk1 != null && self.options.sliderConfig.items_brk1 < total) ? true : false,
-                                fade: false
-                            }
-                        },
-                        {
-                            breakpoint: self.options.breakpoints.breakpoint_2,
-                            settings: {
-                                slidesToShow: (self.options.sliderConfig.items_brk2 >= 0 && self.options.sliderConfig.items_brk2 != null) ? parseInt(self.options.sliderConfig.items_brk2) : parseInt(items),
-                                slidesToScroll: (self.options.sliderConfig.items_brk2 >= 0 && self.options.sliderConfig.items_brk2 != null) ? parseInt(self.options.sliderConfig.items_brk2) : parseInt(items),
-                                arrows: parseInt(self.options.sliderConfig.nav_brk2) == 1 ? true : false,
-                                dots: (self.options.sliderConfig.items_brk2 != null && self.options.sliderConfig.items_brk2 < total) ? true : false,
-                                fade: false
-                            }
-                        },
-                        {
-                            breakpoint: self.options.breakpoints.breakpoint_3,
-                            settings: {
-                                slidesToShow: (self.options.sliderConfig.items_brk3 >= 0 && self.options.sliderConfig.items_brk3 != null) ? parseInt(self.options.sliderConfig.items_brk3) : parseInt(items),
-                                slidesToScroll: (self.options.sliderConfig.items_brk3 >= 0 && self.options.sliderConfig.items_brk3 != null) ? parseInt(self.options.sliderConfig.items_brk3) : parseInt(items),
-                                arrows: parseInt(self.options.sliderConfig.nav_brk3) == 1 ? true : false,
-                                dots: (self.options.sliderConfig.items_brk3 != null && self.options.sliderConfig.items_brk3 < total) ? true : false
-                            }
-                        },
-                        {
-                            breakpoint: self.options.breakpoints.breakpoint_4,
-                            settings: {
-                                slidesToShow: (self.options.sliderConfig.items_brk4 >= 0 && self.options.sliderConfig.items_brk4 != null) ? parseInt(self.options.sliderConfig.items_brk4) : parseInt(items),
-                                slidesToScroll: (self.options.sliderConfig.items_brk4 >= 0 && self.options.sliderConfig.items_brk4 != null) ? parseInt(self.options.sliderConfig.items_brk4) : parseInt(items),
-                                arrows: parseInt(self.options.sliderConfig.nav_brk4) == 1 ? true : false,
-                                dots: (self.options.sliderConfig.items_brk4 != null && self.options.sliderConfig.items_brk4 < total) ? true : false
-                            }
-                        }
-                    ]
-                });
+            options = self.options;
+            
+            self._initializeSlider(options);
         },
+
+        _initializeSlider: function(options) {
+            var self = this,
+                slider = self.element,
+                slConf = options.sliderConfig,
+                items = ((slConf.items >= 0 && slConf.items != null) ? slConf.items : 2),
+                total = options.total,
+                stagePadding = slConf.stagePadding != '' ? parseInt(slConf.stagePadding) : 0,
+                animate_Out = slConf.transition == 'fadeOut' ? true : false;
+                
+            slider.slick({
+                autoplay: parseInt(slConf.autoplay) == 1 ? true : false,
+                autoplaySpeed: (parseInt(slConf.autoplayTimeout) > 0 && slConf.autoplayTimeout != null) ? parseInt(slConf.autoplayTimeout) : 3000,
+                adaptiveHeight: parseInt(slConf.autoHeight) == 1 ? true : false,
+                arrows: parseInt(slConf.nav) == 1 ? true : false,
+                dots: (parseInt(slConf.dots) == 1 && parseInt(items) < total) ? true : false,
+                lazyLoad: parseInt(slConf.lazyLoad) == 1 ? "ondemand" : "progressive",
+                infinite: parseInt(slConf.loop) == 1 ? true : false,
+                centerPadding: parseInt(slConf.center) == 1 ? 0 : stagePadding,
+                slidesToShow: parseInt(items),
+                slidesToScroll: parseInt(items),
+                fade: animate_Out,
+                responsive: [
+                {
+                    breakpoint: self.options.breakpoints.breakpoint_1,
+                    settings: {
+                        slidesToShow: (slConf.items_brk1 >= 0 && slConf.items_brk1 != null) ? parseInt(slConf.items_brk1) : parseInt(items),
+                        slidesToScroll: (slConf.items_brk1 >= 0 && slConf.items_brk1 != null) ? parseInt(slConf.items_brk1) : parseInt(items),
+                        arrows: parseInt(slConf.nav_brk1) == 1 ? true : false,
+                        dots: (slConf.items_brk1 != null && slConf.items_brk1 < total) ? true : false,
+                        fade: false
+                    }
+                },
+                {
+                    breakpoint: self.options.breakpoints.breakpoint_2,
+                    settings: {
+                        slidesToShow: (slConf.items_brk2 >= 0 && slConf.items_brk2 != null) ? parseInt(slConf.items_brk2) : parseInt(items),
+                        slidesToScroll: (slConf.items_brk2 >= 0 && slConf.items_brk2 != null) ? parseInt(slConf.items_brk2) : parseInt(items),
+                        arrows: parseInt(slConf.nav_brk2) == 1 ? true : false,
+                        dots: (slConf.items_brk2 != null && slConf.items_brk2 < total) ? true : false,
+                        fade: false
+                    }
+                },
+                {
+                    breakpoint: self.options.breakpoints.breakpoint_3,
+                    settings: {
+                        slidesToShow: (slConf.items_brk3 >= 0 && slConf.items_brk3 != null) ? parseInt(slConf.items_brk3) : parseInt(items),
+                        slidesToScroll: (slConf.items_brk3 >= 0 && slConf.items_brk3 != null) ? parseInt(slConf.items_brk3) : parseInt(items),
+                        arrows: parseInt(slConf.nav_brk3) == 1 ? true : false,
+                        dots: (slConf.items_brk3 != null && slConf.items_brk3 < total) ? true : false
+                    }
+                },
+                {
+                    breakpoint: self.options.breakpoints.breakpoint_4,
+                    settings: {
+                        slidesToShow: (slConf.items_brk4 >= 0 && slConf.items_brk4 != null) ? parseInt(slConf.items_brk4) : parseInt(items),
+                        slidesToScroll: (slConf.items_brk4 >= 0 && slConf.items_brk4 != null) ? parseInt(slConf.items_brk4) : parseInt(items),
+                        arrows: parseInt(slConf.nav_brk4) == 1 ? true : false,
+                        dots: (slConf.items_brk4 != null && slConf.items_brk4 < total) ? true : false
+                    }
+                }]
+            }, self._showSlider());
+        },
+
+        _showSlider: function() {
+            var slider = this.element;
+            
+            slider.removeClass('slider-hide');
+        }
     });
 
     return $.rw.slider;
